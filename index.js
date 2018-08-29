5fff363cae3030ab06d4b5e0a09ed1d05a2b8bd7
RakeshBairwa <42769885+RakeshBairwa@users.noreply.github.com>
2018-08-29 14:42:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -118,8 +118,9 @@ restService.post("/audio", function(req, res) {
       break;
   }
   return res.json({
-    speech: speech,
-    displayText: speech,
+    speech: "Text response",
+   // "type": 0,
+    //displayText: speech,
     source: "webhook-echo-sample"
   });
 });
